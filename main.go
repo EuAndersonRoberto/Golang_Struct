99e40dd52dbd7d95fafb043fa1928c2df09ac057
EuAndersonRoberto <euandersondigital@gmail.com>
2022-12-12 08:59:22 -0300

    second commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"Golang_Alura_Struct/contas"
+)
+
+func main() {
+	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
+	contadoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
+	
+	status := contaDaSilvia.Transferir(200, &contadoGustavo)
+	
+	fmt.Println(status)
+	fmt.Println(contaDaSilvia)
+	fmt.Println(contadoGustavo)
+}
