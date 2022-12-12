08582b985849051044f38b3b73865c08885e4f1b
EuAndersonRoberto <euandersondigital@gmail.com>
2022-12-12 12:46:34 -0300

    Foi criado o tipo 'interface' onde podemos utilizar ambas as contas para pagar um boleto através da função 'Sacar.'

--- main.go
+++ main.go
@@ -7,18 +7,38 @@ import (
 
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64){
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	contaAnderson := contas.ContaPoupanca{}
-	contaAnderson.Depositar(100)
+	contaAnderson.Depositar(300)
 
 	fmt.Println(contaAnderson)
 
-	contaAnderson.Depositar(200)
+	contaAnderson.Sacar(100.)
 
 	fmt.Println(contaAnderson)
-
-	contaAnderson.Sacar(50.)
+	
+	PagarBoleto(&contaAnderson, 120)
 
 	fmt.Println(contaAnderson.ObterSaldo())
 
-} 
+	contaAna := contas.ContaCorrente{}
+	contaAna.Depositar(400)
+
+	fmt.Println(contaAna)
+
+	contaAna.Sacar(100)
+
+	fmt.Println(contaAna)
+
+	PagarBoleto(&contaAna, 120)
+
+	fmt.Println(contaAna.ObterSaldo())
+	}
